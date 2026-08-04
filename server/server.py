64e32a3025a1744e59d4b09090dbbc744951435a
Shahporan Alif <shahporanalif@gmail.com>
2026-08-04 15:03:11 +0600

    server code

--- server/server.py
+++ server/server.py
@@ -1,9 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
+import os
 import util
 
-app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CLIENT_DIR = os.path.join(BASE_DIR, "..", "Client")
 
+app = Flask(__name__, static_folder=CLIENT_DIR)
+
+# Load model when server starts
 util.load_saved_artifacts()
+
+
+# Serve Frontend
+@app.route("/")
+def home():
+    return send_from_directory(CLIENT_DIR, "index.html")
+
+
+# Serve CSS, JS and other static files
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory(CLIENT_DIR, path)
+
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
@@ -11,10 +29,10 @@ def get_location_names():
         'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
-@app.route('/predict_home_price', methods=['GET', 'POST'])
+
+@app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
@@ -22,13 +40,12 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
+
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
